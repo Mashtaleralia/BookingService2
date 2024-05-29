7e7909e951f165b888724c86a538baedcef930e7
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-05-29 12:08:38 +0300

    header label added to the vc

--- YetAnotherBookingService/ViewController.swift
+++ YetAnotherBookingService/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let headerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.text = "Поиск дешевых авиабилетов"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.textColor = .white
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        view.addSubview(headerLabel)
+        addConstraints()
     }
-
-
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            headerLabel.heightAnchor.constraint(equalToConstant: 100),
+            headerLabel.widthAnchor.constraint(equalToConstant: 172)
+        ])
+    }
+    
+    
+    
+    
 }
 
