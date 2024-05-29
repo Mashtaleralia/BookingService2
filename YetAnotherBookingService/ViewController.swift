e936cc66f30ac40686d9e8db31417b7b68849e5a
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-05-29 13:54:06 +0300

    addinng ticketSearchView to the main screen

--- YetAnotherBookingService/ViewController.swift
+++ YetAnotherBookingService/ViewController.swift
@@ -9,22 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let style: StyleProvider = Style()
+    
     private let headerLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
         label.text = "Поиск дешевых авиабилетов"
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: 22, weight: .semibold)
-        label.textColor = .white
+        label.font = Style().title1
+        label.textColor = Style().textColor
         return label
     }()
     
+    private var ticketSearchView = TicketSearchView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         view.addSubview(headerLabel)
+        view.addSubview(ticketSearchView)
         addConstraints()
     }
     
@@ -33,7 +37,11 @@ class ViewController: UIViewController {
             headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             headerLabel.heightAnchor.constraint(equalToConstant: 100),
-            headerLabel.widthAnchor.constraint(equalToConstant: 172)
+            headerLabel.widthAnchor.constraint(equalToConstant: 172),
+            ticketSearchView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 35),
+            ticketSearchView.heightAnchor.constraint(equalToConstant: 90),
+            ticketSearchView.widthAnchor.constraint(equalToConstant: 296),
+            ticketSearchView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     
