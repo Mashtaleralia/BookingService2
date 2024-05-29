04362b219796f277e3012573adeac58506d33a61
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-05-29 12:08:53 +0300

    ticketSearchView added

---
+++ YetAnotherBookingService/Views/TicketSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  TicketSearchView.swift
+//  YetAnotherBookingService
+//
+//  Created by Admin on 29.05.2024.
+//
+
+import UIKit
+
+class TicketSearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
