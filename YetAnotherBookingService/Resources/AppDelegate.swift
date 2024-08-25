33e9a30790c04e0173c19c64e65ff467cbbd40c2
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-08-25 13:28:49 +0300

    presenting Main vc when app is launched

--- YetAnotherBookingService/Resources/AppDelegate.swift
+++ YetAnotherBookingService/Resources/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let vc = UINavigationController(rootViewController: ViewController())
+        let vc = UINavigationController(rootViewController: MainViewController())
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = vc
         window.makeKeyAndVisible()
